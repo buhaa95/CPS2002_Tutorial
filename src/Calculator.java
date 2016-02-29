58fbaf214a80c6892c6597340dfd1315e977d0eb
Andrew Buhagiar <andrewbuhagiar@gmail.com>
2016-02-29 09:25:51 +0100

    Commit of changes in Calculator.java and Operation.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,6 @@
 
 public class Calculator {
-
+	public int calc(Operation op, int x, int y){
+		return 0;
+	}
 }
--- src/Operation.java
+++ src/Operation.java
@@ -1,4 +1,4 @@
 
 public interface Operation {
-
+	public int perform(int x,int y); //performs operation on x,y
 }
