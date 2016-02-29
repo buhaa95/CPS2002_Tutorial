29668c945f88dc49fd37640207e0d4bfc648bca3
Angie Ann Camilleri <angieanncamilleri@yahoo.com>
2016-02-29 09:51:41 +0100

    updated add

--- src/Add.java
+++ src/Add.java
@@ -1,9 +1,9 @@
-public class Add implements Operation
+.public class Add implements Operation
 {
 
     @Override
     public int perform(int x, int y) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return y+x;
     }
 	
 }
